60a8a30a705b0678064d8b1181db6e6aeae47171
Luto Yvan <zuoez02@outlook.com>
2023-02-23 16:29:56 +0000

    Release v0.2.0: 更新插件系统远程功能

--- src/config.ts
+++ src/config.ts
@@ -1,30 +1,13 @@
 const path = window.require('path');
 
-const getCrossPlatformAppDataFolder = () => {
-    let configFilePath
-    if (process.platform === "darwin") {
-        configFilePath = path.join(
-            window.process.env.HOME,
-            "/Library/Application Support"
-        )
-    } else if (process.platform === "win32") {
-        // Roaming包含在APPDATA中了
-        configFilePath = window.process.env.APPDATA
-    } else if (process.platform === "linux") {
-        configFilePath = window.process.env.HOME
-    }
-    return configFilePath
-};
-
 export const SIYUAN_DATA_PATH = window.siyuan.config.system.dataDir;
 
 export const PLUGIN_FOLDER = 'plugins';
 
-export const VERSION = 'v0.1.1';
+export const VERSION = 'v0.2.0';
 
 export const VERSION_URL = 'https://gitee.com/zuoez02/siyuan-plugin-system/raw/main/VERSION';
 
 export const SCRIPT_URL = 'https://gitee.com/zuoez02/siyuan-plugin-system/raw/main/main.js';
 
 export const PLUGIN_SYS_ABS_PATH = path.join(getCrossPlatformAppDataFolder(), '.siyuan', 'plugin.js');
-
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,5 @@
+const path = window.require('path');
+
 export const log = (...p) => {
     console.log(`[Plugin System] `, ...p)
 };
@@ -5,3 +7,19 @@ export const log = (...p) => {
 export const error = (...p) => console.error(`[Plugin System] `, ...p);
 
 export const reloadWindow = () => window.location.reload();
+
+export const getCrossPlatformAppDataFolder = () => {
+    let configFilePath
+    if (process.platform === "darwin") {
+        configFilePath = path.join(
+            window.process.env.HOME,
+            "/Library/Application Support"
+        )
+    } else if (process.platform === "win32") {
+        // Roaming包含在APPDATA中了
+        configFilePath = window.process.env.APPDATA
+    } else if (process.platform === "linux") {
+        configFilePath = window.process.env.HOME
+    }
+    return configFilePath
+};
