994fe0021c64dcece45c178c857c38edbccf1bce
zuoez02 <zuoez02@outlook.com>
2023-02-25 07:17:39 +0000

    fix: v0.3.4

--- src/internal/plugins/views/setting-list.svelte
+++ src/internal/plugins/views/setting-list.svelte
@@ -4,11 +4,15 @@
     import { container } from "../../../inversify";
     import { TYPES } from "../../../types";
     import { PluginSystem } from "../../../plugin";
+    import { IPlugin } from "../../../type.d";
 
     const storageManager = container.get<StorageManager>(TYPES.StorageManager);
     const pluginSystem = container.get<PluginSystem>(TYPES.PluginSystem);
 
-    let plugins = [];
+    let plugins: IPlugin[] = [];
+
+    $: internalPlugins = plugins.filter((p) => p.plugin);
+    $: outsidePlugins = plugins.filter((p) => p.script);
 
     const loadPlugins = () => {
         plugins = storageManager.getPlugins();
@@ -29,21 +33,54 @@
     });
 </script>
 
-{#each plugins as plugin}
-    {#if !plugin.hidden}
-        <label class="fn__flex b3-label">
-            <div class="fn__flex-1">
-                {plugin.name}
-                <div class="b3-label__text">{plugin.description || "无描述内容"}</div>
-            </div>
-            <span class="fn__space" />
-            <input
-                class="b3-switch fn__flex-center"
-                id="fullWidth"
-                type="checkbox"
-                bind:checked={plugin.enabled}
-                on:change={onPluginEnabledChange(plugin.key)}
-            />
-        </label>
-    {/if}
-{/each}
+<label class="b3-label fn__flex">
+    <div class="fn__flex-1">
+        内置插件
+        {#each internalPlugins as plugin}
+            {#if !plugin.hidden}
+                <label class="fn__flex b3-label">
+                    <div class="fn__flex-1">
+                        {plugin.name}
+                        <div class="b3-label__text">
+                            {plugin.description || "无描述内容"}
+                        </div>
+                    </div>
+                    <span class="fn__space" />
+                    <input
+                        class="b3-switch fn__flex-center"
+                        id="fullWidth"
+                        type="checkbox"
+                        bind:checked={plugin.enabled}
+                        on:change={onPluginEnabledChange(plugin.key)}
+                    />
+                </label>
+            {/if}
+        {/each}
+    </div>
+</label>
+
+<label class="b3-label fn__flex">
+    <div class="fn__flex-1">
+        第三方插件
+        {#each outsidePlugins as plugin}
+            {#if !plugin.hidden}
+                <label class="fn__flex b3-label">
+                    <div class="fn__flex-1">
+                        {plugin.name}
+                        <div class="b3-label__text">
+                            {plugin.description || "无描述内容"}
+                        </div>
+                    </div>
+                    <span class="fn__space" />
+                    <input
+                        class="b3-switch fn__flex-center"
+                        id="fullWidth"
+                        type="checkbox"
+                        bind:checked={plugin.enabled}
+                        on:change={onPluginEnabledChange(plugin.key)}
+                    />
+                </label>
+            {/if}
+        {/each}
+    </div>
+</label>
